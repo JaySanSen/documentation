e9a939d21d4b92f03cfef8508d5c26c932e9692e
JaySanSen <183524427+JaySanSen@users.noreply.github.com>
2025-11-03 01:24:48 +0530

    updated gitignore

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,8 +4,8 @@ public class App {
     public String getGreeting() {
         return "Hello World!";
     }
+
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-        String testString = "Hello world";
+        System.out.println("Hello World");
     }
 }
